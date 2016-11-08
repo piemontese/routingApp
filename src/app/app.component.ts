667a7ef7c93473817934b41d719009f224efcc0c
= <pie.piemontese@gmail.com>
2016-11-08 13:58:43 +0100

    Hide appComponent in other component
    appComponent is shown only in appComponent template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute} from '@angular/router';    
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  private myUrl:any;
+  private show:boolean;
+    
+  constructor(/*private router: Router, */private route:ActivatedRoute) {
+     this.route.url.subscribe(
+     (data: any) => {
+        this.show = true;
+        for (let i of data) {
+          this.myUrl = i.path;
+          this.show = false;
+          // ... get whatever you want
+        }
+        console.log("My Current Url ", this.myUrl);
+        console.log("There is something more: ",data);
+        console.log("show = ", this.show);
+     },
+     (error: any) => console.debug("URL ERROR", error));
+  }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  private myUrl:any;
+    
+  constructor( private route:ActivatedRoute ) { 
+     this.route.url.subscribe(
+     (data: any) => {
+         for (let i of data) {
+            this.myUrl = i.path;
+            // ... get whatever you want
+         }
+//         console.log("My Current Url ", this.myUrl);
+//         console.log("There is something more: ",data);
+     },
+     (error: any) => console.debug("URL ERROR", error));
+  }
 
   ngOnInit() {
   }
